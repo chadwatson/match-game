636e531f1696a94b47862753bd77cd3fdb39b712
Chad Watson <chadwatsn@gmail.com>
2025-02-01 15:57:30 -0600

    Improves mobile layout

--- app/game/game.tsx
+++ app/game/game.tsx
@@ -95,10 +95,10 @@ export default function Game(props: {
 
   return (
     <form className="w-screen h-screen">
-      <header className="absolute top-0 left-0 right-0 h-16 flex items-center justify-between px-6 py-2">
+      <header className="absolute top-0 left-0 right-0 h-16 flex items-center justify-between px-2 lg:px-6 py-2">
         <div className="flex-1">
           <div className="flex items-center">
-            <div className="mr-4 pr-6 flex items-center">
+            <div className="mr-2 pr-3 md:mr-4 md:pr-6 flex items-center">
               <div
                 className={`rounded w-10 h-10 mr-3 flex items-center justify-center font-bold text-2xl ${
                   playerOneWins || (!gameOver && currentPlayer === 1)
@@ -121,7 +121,7 @@ export default function Game(props: {
                 </div>
               </div>
             </div>
-            <div className="mr-4 pr-6 flex items-center">
+            <div className="mr-2 pr-3 md:mr-4 md:pr-6 flex items-center">
               <div
                 className={`rounded w-10 h-10 mr-3 flex items-center justify-center font-bold text-2xl ${
                   playerTwoWins || (!gameOver && currentPlayer === 2)
@@ -150,30 +150,30 @@ export default function Game(props: {
           {matchFound ? (
             <button
               type="button"
-              className="cursor-pointer rounded-full inline-block py-1 pl-3 pr-4 font-bold bg-emerald-700 text-sm text-white text-nowrap hover:bg-emerald-800 border-b-4 border-emerald-800"
+              className="cursor-pointer rounded-full inline-block px-2 md:px-3 py-1 md:py-1 font-bold bg-emerald-700 text-sm text-white text-nowrap hover:bg-emerald-800 border-b-4 border-emerald-800"
               onClick={collectCards}
             >
               <span className="flex items-center">
-                <CheckIcon className="size-5 mr-1" />
-                <span>Match Found!</span>
+                <CheckIcon className="size-7 md:size-5 md:mr-1" />
+                <span className="hidden md:inline-block">Match Found!</span>
               </span>
             </button>
           ) : guessesMade ? (
             <button
               type="button"
-              className="rounded-full inline-block py-1 pl-3 pr-4 cursor-pointer font-bold bg-violet-800 text-white text-sm text-nowrap hover:bg-violet-900 border-b-4 border-violet-900"
+              className="rounded-full inline-block px-2 md:px-3 py-1 md:py-1 cursor-pointer font-bold bg-violet-800 text-white text-sm text-nowrap hover:bg-violet-900 border-b-4 border-violet-900"
               onClick={switchPlayer}
             >
               <span className="flex items-center">
-                <ArrowPathIcon className="size-5 mr-1" />
-                <span>Switch</span>
+                <ArrowPathIcon className="size-7 md:size-5 md:mr-1" />
+                <span className="hidden md:inline-block">Switch</span>
               </span>
             </button>
           ) : null}
           {!gameOver && (
             <button
               type="button"
-              className="inline-block rounded-full cursor-pointer px-3 py-1 ml-4 text-sm font-bold bg-gray-50 text-gray-700 hover:bg-gray-200 border-b-4 border-gray-400 hover:text-gray-900 active:border-0"
+              className="inline-block rounded-full cursor-pointer px-2 md:px-3 py-1 md:py-1 ml-2 md:ml-4 text-sm font-bold bg-gray-50 text-gray-700 hover:bg-gray-200 border-b-4 border-gray-400 hover:text-gray-900 active:border-0"
               onClick={() => {
                 if (confirm("Are you sure you want to start a new game?")) {
                   router.push("/");
@@ -181,28 +181,28 @@ export default function Game(props: {
               }}
             >
               <span className="flex items-center justify-center">
-                <SparklesIcon className="size-5 mr-1" />
-                New Game
+                <SparklesIcon className="size-7 md:size-5 md:mr-1" />
+                <span className="hidden md:inline-block">New Game</span>
               </span>
             </button>
           )}
         </div>
       </header>
-      <div className="w-screen h-screen pt-16 p-6">
+      <div className="w-screen h-screen pt-16 px-2 pb-2 lg:px-6 lg:pb-6">
         {gameOver ? (
           <Suspense>
             <GameOver winner={playerOneWins ? 1 : 2} />
           </Suspense>
         ) : (
           <div
-            className={`w-full h-full max-w-full max-h-full grid gap-6 ${gridClassName(
+            className={`w-full h-full max-w-full max-h-full grid gap-2 lg:gap-6 ${gridClassName(
               props.difficulty
             )}`}
           >
             {[...deck].map((image, index) => (
               <div key={`card-${index}`} className="w-full h-full relative">
                 {foundCards.has(index) ? (
-                  <div className="block w-full h-full border border-gray-800 rounded-lg overflow-hidden" />
+                  <div className="block w-full h-full border border-gray-800 rounded lg:rounded-lg overflow-hidden" />
                 ) : (
                   <>
                     <input
