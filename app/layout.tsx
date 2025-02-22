4f3cf98fe5063475338da6070312709d8f3b3769
Chad Watson <chadwatsn@gmail.com>
2025-02-22 15:15:17 -0600

    Adds clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata, Viewport } from "next";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -12,8 +20,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="antialiased">
+          <header className="flex justify-between items-center p-4 h-16">
+            <h1 className="font-bold text-lg">Match & Match</h1>
+            <div className="flex justify-end items-center gap-4">
+              <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
