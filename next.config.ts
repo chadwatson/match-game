c87bae752e81383a3e3e2d11f808a57009407d1e
Chad Watson <chadwatsn@gmail.com>
2025-02-21 23:24:39 -0600

    Adds blob and neon APIs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xb2hfdigjsaryaxv.public.blob.vercel-storage.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
